c8dd076e7d540ec79b027098e4753e51d3425c42
Student6 <Student6@10.8.33.167>
2017-01-12 13:15:47 -0500

    dfsdfsdfwsdf

--- SimonHunter/src/partnerCodeInHerePlease/Button.java
+++ SimonHunter/src/partnerCodeInHerePlease/Button.java
@@ -1,20 +1,23 @@
 package partnerCodeInHerePlease;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 
 import gui.components.Action;
+import gui.components.Component;
 import simonHunter.ButtonInterfaceHunter;
 
-public class Button implements ButtonInterfaceHunter {
+public class Button extends Component implements ButtonInterfaceHunter {
 
 	private Color color;
 	private Action action;
-	private int y;
-	private int x;
 	
 	public Button() {
-		super();
+		super(50, 50, 120, 50);
 	}
 
 	@Override
@@ -30,84 +33,73 @@ public class Button implements ButtonInterfaceHunter {
 	}
 
 	@Override
-	public int getHeight() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public BufferedImage getImage() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int getWidth() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int getX() {
-		return this.x;
-	}
-
-	@Override
-	public int getY() {
-		return this.y;
-	}
-
-	@Override
-	public boolean isAnimated() {
-		return false;
-	}
-
-	@Override
-	public void update() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
 	public void setName(String string) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
-	public void setColor(Color color) {
-		// TODO Auto-generated method stub
-
+	public void setColor(Color c) {
+		color = c;
+		update();
 	}
 
 	@Override
 	public void dim() {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
 	public void setAction(Action action) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
 	public void highlight() {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
 	public void setX(int i) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
 	public void setY(int i) {
 		// TODO Auto-generated method stub
-
+		
 	}
+
+	@Override
+	public void update(Graphics2D g) {
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		g.setColor(color);
+		g.fillRoundRect(0, 0, getWidth(), getHeight(), 35, 25);
+		g.setColor(Color.black);
+		g.drawRoundRect(0, 0, getWidth()-1, getHeight()-1, 35, 25);
+//		g.setFont(new Font(getFont(),Font.PLAIN,getSize()));
+//		FontMetrics fm = g.getFontMetrics();
+//		
+//		if(getText()!= null){
+//			g.setColor(Color.white);
+//			String t = getText();
+//			//just in case text is too wide, cut off
+//			int cutoff = t.length();
+//			while(cutoff > 0 && fm.stringWidth(t) > getWidth()){
+//				cutoff --;
+//				t = t.substring(0,cutoff); 
+//			}
+//			g.drawString(t, (getWidth()-fm.stringWidth(t))/2, 
+//					(getHeight()+fm.getHeight()-fm.getDescent())/2);
+//		}
+		
+	}
+
+	
 
 }
